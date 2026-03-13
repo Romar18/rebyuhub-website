8c1a3fd1c078c174702299015d7d66bf6a98f5c2
Romar18 <101159738+Romar18@users.noreply.github.com>
2026-03-14 01:59:52 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static("public", { fallthrough: true }));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"))
+})
+
 // Setup multer for file uploads
 const uploadDir = "public/uploads";
 if (!fs.existsSync(uploadDir)) {
